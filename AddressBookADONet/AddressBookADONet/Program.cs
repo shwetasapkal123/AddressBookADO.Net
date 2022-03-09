1f9e80914e2161c1292f7f3076b59b7c3c8c010c
shwetasapkal123 <sapkalshweta30797@gmail.com>
2022-03-09 17:22:25 +0530

    [shweta] Wel-come message

---
+++ AddressBookADONet/AddressBookADONet/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookADONet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WEL_COME to address book Ado,net");
+            Console.ReadLine();
+        }
+    }
+}
