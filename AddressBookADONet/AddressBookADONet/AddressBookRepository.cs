0dbe2c95566813d2a8eb384c37ac684c824d5663
shwetasapkal123 <sapkalshweta30797@gmail.com>
2022-03-10 21:56:53 +0530

    [shweta] Created AddressBook Table

--- AddressBookADONet/AddressBookADONet/AddressBookRepository.cs
+++ AddressBookADONet/AddressBookADONet/AddressBookRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,8 @@ namespace AddressBookADONet
 {
     public class AddressBookRepository
     {
+        public static string connectionString = @"Data Source=DESKTOP-QGMU8R3;Initial Catalog=Address_Book;Integrated Security=True";
+        //Represents a connection to Sql Server Database
+        SqlConnection sqlConnection = new SqlConnection(connectionString);
     }
 }
