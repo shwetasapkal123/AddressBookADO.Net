f7ce7dde79799296f9c423ed6296c8ebe4c8764e
shwetasapkal123 <sapkalshweta30797@gmail.com>
2022-03-10 21:49:14 +0530

    [shweta] Created AddressBook Database

---
+++ AddressBookADONet/AddressBookADONet/AddressBookModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookADONet
+{
+    public class AddressBookModel
+    {
+        public int Id { get; set; }
+        public string first_Name { get; set; }
+        public string last_Name { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string email { get; set; }
+        public int zip { get; set; }
+        public long phoneNumber { get; set; }
+
+    }
+}
---
+++ AddressBookADONet/AddressBookADONet/AddressBookRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookADONet
+{
+    public class AddressBookRepository
+    {
+    }
+}
